fb69ae1513bd4e4ab523c60c6b19a1ef51ff8dbb
Filip Ciepiela <filip.ciepiela@gmail.com>
2016-09-16 10:59:08 +0200

    "comments"

--- standardAddition/standardAdditionSlope.m
+++ standardAddition/standardAdditionSlope.m
@@ -1,9 +1,14 @@
-function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( DATACELL, peakLocation, options )
-%%
-% DATACELL has four cell arrays:
+function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope(DATACELL, peakLocation, options)
+%
+% standardAdditionSlope(DATACELL, peakLocation, options) is a function which tries
+% to perform standard addition analysis using peak slope at the inflection points
+% without any background correction. 
+%
+%
+% DATACELL has four cell arrays (can be generated via prepareStructFromRawData):
 %	Y: MATRIX with registered signals in each column
 %	X: VECTOR with values to plot Y(:,i) against (the same number of rows as Y, and one
-%	column)
+%	column) -- used only for plots.
 %	CONC: VECTOR with values of concentration for each column of Y (so the same
 %	number of columns and one row)
 %	SENS: VECTOR with the INDEX (so 1:nrOfSens values) of sensitivity of each Y column (so the same
@@ -15,16 +20,43 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 %	of 100 data points peakLocation can be a number between 1 and 100.
 %
 % options is a structure, which may contain fields:
-% .average - boolean, true means that columns with the same concentration
-%			and sensitivity should be everaged
-% .smooth - boolean, true means that data should be smoothed before further
-%			processing
-% .forceSamePoints - boolean, true means that inflection points should only
-%			be calculated for the curve with highest concentration 
-%			and used for all (as opposed to calculating the
-%			inflection point for all curves independiently.
+%	  .average - boolean, true means that columns with the same concentration
+%				and sensitivity should be everaged
+%	  .smooth - boolean, true means that data should be smoothed before further
+%				processing
+%	  .forceSamePoints - boolean, true means that inflection points should only
+%				be calculated for the curve with highest concentration 
+%				and used for all (as opposed to calculating the
+%				inflection point for all curves independiently.
+%
+% Copyright (c) 2016, Filip Ciepiela <filip.ciepiela@agh.edu.pl> 
+% All rights reserved.
+%
+%
+% BSD-like license.
+% Redistribution and use in source and binary forms, with or without modification, 
+% are permitted provided that the following conditions are met:
+%
+% Redistributions of source code must retain the above copyright notice, 
+% this list of conditions and the following disclaimer.
+%
+% Redistributions in binary form must reproduce the above copyright notice,
+% this list of conditions and the following disclaimer in the documentation 
+% and/or other materials provided with the distribution.
+%
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+% AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER 
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+% IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+% OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %
 
+% Check input and set some default values (this may be tweaked)
+%==============================================================
     correlationTreshhold = 0.8;
     rowRemoveTresholdPercent = 0.3;
 	slopeDiffRequired = 0.05;
@@ -56,6 +88,8 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 	sens = DATACELL.SENS;
 	
 	if ( options.average == true )
+        % try to perform data averaging by sensitivity && concentration
+        %==============================================================
 		disp('Averaging curves with the same concentration and sensitivity');
 		pos=1;
 		for s=unique(sens)
@@ -79,19 +113,32 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 		dataY = newDataY;
 		conc = newConc;
 		sens = newSens;
-	end
-
+    end
+    
+    % sort data by conc, so it is easier to follow the work flow
+    %===========================================================
 	[ concSort, sortOrder ] = sort(conc);
 	sensSort = sens(1,sortOrder);
 	dataYSort = zeros(size(dataY));
 	dataYSort(:,:) = dataY(:,sortOrder);
 
-	if ( options.smooth ) 
+	if ( options.smooth )
+        % smooth is required
+        %===================
 		for i=1:size(dataYSort,2)
 			dataYSort(:,i) = smooth(dataYSort(:,i));
 		end
-	end
+    end
 
+    % Very important step -- find the slope of each peak in inflection point. 
+    % By default it reuses the point where the slope was taken from the peak
+    % with highest concentration -- if the location of the peak 
+    % moves with concetration please use:
+    % options.forceSamePoints = false;
+    % Variables may end with R, L or AVG, which means they deal with
+    % rigth, left or average slope in inflection point respectivly.
+    % getSlopeInInflection is included in this file.
+    %================================================================
 	for i=size(dataYSort,2):-1:1
 		if ( i == size(dataYSort,2) )
 			[slopeL(1,i), slopeR(1,i), slopeAVG(1,i), fitRange(:,i)] = getSlopeInInflection(dataYSort(:,i), peakLocation, false);
@@ -100,9 +147,8 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 		end
 	end
 	
-	%
-	% NORMAL FITS:
-	%
+	% It uses Normal Equation to find the optimal fit of calibration plot.
+	%=====================================================================
 	figure;
 	hold on;
 	icons = [ 'o' '+' '*' 's' 'd' 'v' '^' '<' '>' 'p' 'h' ];
@@ -133,12 +179,17 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
     calibration.R = slopeR(1,:);
     calibration.AVG = slopeAVG(1,:);
 	
+    % Generate initial matrix of intercepts
+    %======================================
 	crossL = zeros(size(normalFitL,2),size(normalFitL,2),2);
 	crossR = zeros(size(normalFitR,2),size(normalFitR,2),2);
 	crossAVG = zeros(size(normalFitAVG,2),size(normalFitAVG,2),2);
+    
 	rpos = 1;
 	fres = [];
-	
+    
+    % We need to verify if slopes for each peak are found
+    %====================================================
 	avOK = true;
 	rOK = true;
 	lOK = true;
@@ -153,18 +204,22 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
     
 	for i=1:size(normalFitAVG,2)
 		for ii=1:size(normalFitAVG,2)
-			% Check if sensitivities are different enough
 			if ( i >= ii )
                 crossAVG(i,ii,1) = NaN;
                 crossR(i,ii,1) = NaN;
                 crossL(i,ii,1) = NaN;
 				continue;
-			end
+            end
+            % Note that it finds the intersect point for two lines at the
+            % time, filling upper triangle of the matrix
+            %============================================================
 			if ( avOK )
 				prop = normalFitAVG(2,i) / normalFitAVG(2,ii);
 				if ( prop > (1+slopeDiffRequired) || prop < (1-slopeDiffRequired) )
-					% MATRIX solution of intersection points for each
-					% combination
+                    % Check if sensitivities are different enough, to small
+                    % difference will result in serious problem with precission
+                    % (and possible accuracy) of the result
+                    %==========================================================
 					crossAVG(i,ii,:) = pinv([normalFitAVG(2,i) -1; normalFitAVG(2,ii) -1]) * [-normalFitAVG(1,i);-normalFitAVG(1,ii)];
 					plot([ crossAVG(i,ii,1) concSort(end) ], [ crossAVG(i,ii,1) concSort(end) ].*normalFitAVG(2,i) + normalFitAVG(1,i), 'g-');
 					fresAVG(rpos) = -crossAVG(i,ii,1);
@@ -176,11 +231,11 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 				end
             end
 			
+            % The same as above, but for L
+            %=============================
 			if ( lOK )
 				prop = normalFitL(2,i) / normalFitL(2,ii);
 				if ( prop > (1+slopeDiffRequired) || prop < (1-slopeDiffRequired) )
-					% MATRIX solution of intersection points for each
-					% combination
 					crossL(i,ii,:) = pinv([normalFitL(2,i) -1; normalFitL(2,ii) -1]) * [-normalFitL(1,i);-normalFitL(1,ii)];
 					plot([ crossL(i,ii,1) concSort(end) ], [ crossL(i,ii,1) concSort(end) ].*normalFitL(2,i) + normalFitL(1,i), 'b-');
 					fresL(rpos) = -crossL(i,ii,1);
@@ -190,13 +245,13 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 					disp('Sensitivities are too similar for LEFT');
 					lOK = false;
 				end
-			end
+            end
 			
+            % The same as above but for R
+            %============================
 			if ( rOK )
 				prop = normalFitR(2,i) / normalFitR(2,ii);
 				if ( prop > (1+slopeDiffRequired) || prop < (1-slopeDiffRequired) )
-					% MATRIX solution of intersection points for each
-					% combination
 					crossR(i,ii,:) = pinv([normalFitR(2,i) -1; normalFitR(2,ii) -1]) * [-normalFitR(1,i);-normalFitR(1,ii)];
 					plot([ crossR(i,ii,1) concSort(end) ], [ crossR(i,ii,1) concSort(end) ].*normalFitR(2,i) + normalFitR(1,i), 'r-');
 					fresR(rpos) = -crossR(i,ii,1);
@@ -212,26 +267,32 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 		end
     end
     
+    % Here, is a little dirty trick, to remove the intersection points
+    % which are too far from average. It is done by the means of
+    % Coefficient of Variance value, and can be tweaked in the setting at
+    % the beggining of the file.
+    % minimizeCV is included in this file.
+    %====================================================================
     [ crossAVG, removedORDER ] = minimizeCV(squeeze(crossAVG(:,:,1)), 3, rowRemoveTresholdPercent);
     for i=1:length(removedORDER)
         correlation.AVG(removedORDER(i)) = [];
     end
     fresAVG = -crossAVG(logical(triu(ones(size(crossAVG)),1)));
-    stdAVG = std(fresAVG)
+    stdAVG = std(fresAVG);
     
     [ crossR, removedORDER ] = minimizeCV(squeeze(crossR(:,:,1)), 3, rowRemoveTresholdPercent);
     for i=1:length(removedORDER)
         correlation.R(removedORDER(i)) = [];
     end
     fresR = -crossR(logical(triu(ones(size(crossR)),1)));
-    stdR = std(fresR)
+    stdR = std(fresR);
     
      [ crossL, removedORDER ] = minimizeCV(squeeze(crossL(:,:,1)), 3, rowRemoveTresholdPercent);
     for i=1:length(removedORDER)
         correlation.L(removedORDER(i)) = [];
     end
     fresL = -crossL(logical(triu(ones(size(crossL)),1)));
-    stdL = std(fresL)
+    stdL = std(fresL);
     
     disp('Corelations Left:')
     for i=1:size(correlation.L,2)
@@ -248,6 +309,9 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
         disp(correlation.AVG(i));
     end
 	
+    % Here it selects, if it possible to offer the final result, for which
+    % set of data, the result is the best (left, right or average)
+    %=====================================================================
 	if ( lOK && min(correlation.L) > correlationTreshhold  ...
 	&& ( isnan(stdAVG) || stdL <= stdAVG || min(correlation.AVG) <= correlationTreshhold ) ...
 	&& ( isnan(stdR) || stdL <= stdR || min(correlation.R) <= correlationTreshhold ) )
@@ -263,63 +327,73 @@ function [ fres, correlation, calibration, fitRange ] = standardAdditionSlope( D
 		fres = fresAVG;
 	else 
 		error('Could not select slope for calibration, please verify the data');
-	end
-	
-	disp(sprintf('mean: %0.6e', mean(fres)));
-	disp(sprintf('median: %0.6e ',median(fres)));
-	disp(sprintf('the result of calculation: %0.6e ± %0.6e',mean(fres), (std(fres)/sqrt(numel(fres))*tinv(1-(.05/2),length(fres)-1))));
+    end
+
+	disp(sprintf('Partial results median: %0.6e ',median(fres)));
+	disp(sprintf('The final result of standard addition: %0.6e ± %0.6e',mean(fres), (std(fres)/sqrt(numel(fres))*tinv(1-(.05/2),length(fres)-1))));
 
 end
 
 function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(signal, peak, forceFitRange, fitRange )
-	fitSize = 5;
-	maxHit = 4;
-	hitCnt = 0;
-	verbose = true;
+% I find this as one of the most important steps, I has gone trou many
+% iterations, so the code is a bit of mixture of different ideas.
+% Some tweakalbe setting:
+%======================================================================
+	fitSize = 5; %How many points should be fitted to get slope%
+	maxHit = 4;  %How many times the slope has to change to call it inflection point%
+	verbose = true; %Draw some additional plots%
+    
+    % Prepare data structures:
+    %=========================
+    hitCnt = 0;
 	sigLen = length(signal);
 	prevNormalFit = [ NaN NaN ];
 	finalNormalFit = [ NaN NaN ];
 	%signal = smooth(signal,13,'sgolay',3);
 	signal = smooth(signal,17,'sgolay',3);
 	
-	%
-	% FITTING LEFT
-	%
-	% normal equation fit:
-	
 	if ( forceFitRange ) 
-		fitRange;
+        % If we have already the point range in which the slope has to be
+        % found (i.e. this is not the plot with the highest concentration)
+        %=================================================================
 		[blackhole,pos] = max(isnan(fitRange));
 		fitrangeL = fitRange(1:pos-1);
 		fitrangeR = fitRange(pos+1:end);
 
-		% Left
+		% Get linear fit on the left side
+        %===========================
 		X = [ ones(fitSize,1) (1:1:fitSize)' ];
 		normalFitX = pinv( X' * X );
 		normalFitX = normalFitX * X';
 		yL = signal(fitrangeL,1);
 		normalFitL = normalFitX * yL;
 		
-		%Right
+		% Get linear fit on the rigth side
+        %===========================
 		yR = signal(fitrangeR,1);
 		normalFitR = normalFitX * yR;
 
+        % Get slopes:
+        %======================
 		slopeL = normalFitL(2);
 		slopeR = normalFitR(2);
 		
 	else
-		%
-		% FIT LEFT
-		%
+		% This is either the plot with the highest concentration or
+        % option.forceSamePoints == false
+        % So first, we need to find the inflection point, and then
+        % compute its slope.
+        %==========================================================
 		X = [ ones(fitSize,1) (1:1:fitSize)' ];
 		normalFitX = pinv( X' * X );
 		normalFitX = normalFitX * X';
 		fitrange = [];
-		
 % 		if ( verbose )
 % 			figure;
 % 		end
 		for fitPos = peak:-1:fitSize
+            % We start from the left side of the peak:
+            %=========================================
 			if ( peak < fitPos-fitSize )
 				error('error');
 			end
@@ -356,15 +430,14 @@ function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(sig
 		else
 			slopeL = finalNormalFit(2);
 			fitrangeL = finalFitrange;
-		end
-		%
-		% FITTING RIGHT
-		%
+        end
 		fitrange = [];
 % 		if ( verbose )
 % 			figure;
 % 		end
 		for fitPos = peak:1:(sigLen-fitSize)
+            % And now rigth side:
+            %====================
 			fitrange= [ fitPos :1: (fitPos+fitSize-1) ];
 			y = signal(fitrange,1);
 			normalFit = normalFitX * y;
@@ -400,9 +473,11 @@ function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(sig
 		fitRange = [ fitrangeL NaN fitrangeR ];
 	end %end catch
 	
-	%
-	% EXP
-	%
+	% This part provide alternative method of getting the slope in the
+	% infleciton, as any noise will make it much more difficult, here are
+	% tested approaches. This overrites previous method, as is more
+	% difficult to follow.
+    %=====================================================================
 	experimental = 1;
 
 	if ( experimental == 1 )
@@ -434,8 +509,10 @@ function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(sig
 				vr = abs(d2(i));
 				pr = i;
 			end
-		end
-		
+        end
+        
+        %Set final values:
+        %=================
 		if ( pl ~= 0 ) 
 			slopeL = d1(pl);
 		else
@@ -447,12 +524,11 @@ function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(sig
 			slopeR = NaN;
 		end
 			
-	end
-	%
-	%range2
-	%
+    end
 
-	slopeAVGfitRange = (slopeL + -1*slopeR)/2;
+    % Get average slope (rigth slope has negative slope (or should have):
+    %==================================================================
+	slopeAVGfitRange = (slopeL - slopeR)/2;
 	
 	if ( verbose )
 		plot(signal); hold on;
@@ -463,7 +539,9 @@ function [slopeL, slopeR, slopeAVGfitRange, fitRange] = getSlopeInInflection(sig
 end
 
 function [ matrixNANDIAG, removedORDER ] = minimizeCV( matrixNANDIAG, minEntities, minCVchange)
-
+    % We can try 3-sigma here, however I think, this needs to be more
+    % tweakable:
+    %================================================================
     removedORDER = [];
     while ( size(matrixNANDIAG,1) > minEntities )
         inrow = inrows(matrixNANDIAG);
@@ -476,7 +554,6 @@ function [ matrixNANDIAG, removedORDER ] = minimizeCV( matrixNANDIAG, minEntitie
         newinrow = inrows(newmatrixNANDIAG);
         newstd = std(newinrow(:));
         newmeancvtotal = newstd / abs(mean(mean(newinrow)));
-        %(oldmeancvtotal-newmeancvtotal)
         if ( (oldmeancvtotal-newmeancvtotal) > minCVchange )
             matrixNANDIAG = newmatrixNANDIAG;
             removedORDER = [ removedORDER mpos ];
